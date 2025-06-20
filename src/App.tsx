22d01a379205ea8e1218559b845c6b411a333417
romankarbivskyi <romankarbivskyi1@gmail.com>
2025-06-20 12:59:31 +0300

    refactor: simplify App component structure by removing unnecessary div wrapper around Header and Hero components

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,9 @@ import { Header, Hero } from "./sections";
 const App = () => {
   return (
     <main className="bg-dark relative min-h-screen w-full overflow-hidden text-white">
-      <div className="relative z-10">
-        <Header />
-        <Hero />
-      </div>
-      <div className="absolute top-0 left-1/2 z-0 h-1/2 w-full max-w-7xl -translate-1/2 rounded-full bg-radial from-[#5EB0EF]/50 via-[#5EB0EF]/10 to-transparent blur-3xl"></div>
+      <Header />
+      <Hero />
+      <div className="absolute top-0 left-1/2 h-1/2 w-full max-w-7xl -translate-1/2 rounded-full bg-radial from-[#5EB0EF]/50 via-[#5EB0EF]/10 to-transparent blur-3xl"></div>
     </main>
   );
 };
