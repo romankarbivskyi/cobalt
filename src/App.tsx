b483d23bea8e244b690ee03710a37b66fc7302d1
romankarbivskyi <romankarbivskyi1@gmail.com>
2025-06-20 14:02:13 +0300

    refactor: wrap Header and Hero components in a div for improved structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import { About, Header, Hero } from "./sections";
 const App = () => {
   return (
     <main className="bg-dark relative min-h-screen w-full overflow-hidden text-white">
-      <Header />
-      <Hero />
+      <div className="relative z-10">
+        <Header />
+        <Hero />
+      </div>
       <div className="absolute top-0 left-1/2 h-1/2 w-full max-w-7xl -translate-1/2 rounded-full bg-radial from-[#5EB0EF]/50 via-[#5EB0EF]/10 to-transparent blur-3xl"></div>
       <About />
     </main>
