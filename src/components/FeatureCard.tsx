f0f487c5fbd29f1c10e897b2fc108a0200c54e64
romankarbivskyi <romankarbivskyi1@gmail.com>
2025-06-22 12:59:22 +0300

    fix: adjust layout and image styling in FeatureCard component for improved responsiveness

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -12,14 +12,10 @@ const FeatureCard = ({ className, title, subtitle, img }: FeatureCardProps) => {
     <div
       className={cn(
         className,
-        "bg-card border-outline hover:border-accent flex h-[480px] flex-col justify-between rounded-2xl border transition-all duration-300 hover:scale-105",
+        "bg-card border-outline hover:border-accent mx-5 flex flex-col justify-between rounded-2xl border transition-all duration-300 hover:scale-105 sm:mx-10 md:mx-0",
       )}
     >
-      <img
-        src={img}
-        alt={title}
-        className="h-full max-h-[324px] w-full object-contain"
-      />
+      <img src={img} alt={title} className="h-full w-full object-contain" />
       <div className="m-8 space-y-4">
         <h3 className="text-lg font-medium text-[#E4E4E7]">{title}</h3>
         <p className="text-regular max-w-[480px] text-sm">{subtitle}</p>
